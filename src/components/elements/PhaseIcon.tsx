52e1f4deab7aaefd6106aafa449accf9a9285684
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-01-28 10:40:01 +0900

    PhaseIcon.tsxの色を変更

--- src/components/elements/PhaseIcon.tsx
+++ src/components/elements/PhaseIcon.tsx
@@ -41,11 +41,19 @@ const Read = ({ state }: IconProps) => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
-                stroke={state ? "var(--color-yellow)" : "#C7D2DB"}
+                stroke={
+                    state ? "var(--color-yellow)" : "var(--color-light-blue)"
+                }
                 width="60%"
                 height="60%"
             >
-                <g fill={state ? "#FFC80B" : "#C7D2DB"}>
+                <g
+                    fill={
+                        state
+                            ? "var(--color-yellow)"
+                            : "var(--color-light-blue)"
+                    }
+                >
                     <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
@@ -67,7 +75,7 @@ const Delete = ({ state }: IconProps) => {
             height="50%"
         >
             <path
-                fill={state ? "#E54671" : "#C7D2DB"}
+                fill={state ? "var(--color-red)" : "var(--color-light-blue)"}
                 d="M17.25 18H22v2h-6.75zm-12.5 2l-2.125-2.125q-.575-.575-.587-1.425T2.6 15l11-11.4q.575-.6 1.413-.6t1.412.575L21.4 8.55q.575.575.575 1.425T21.4 11.4L13 20z"
             />
         </svg>
@@ -84,7 +92,7 @@ const Fix = ({ state }: IconProps) => {
             height="50%"
         >
             <path
-                fill={state ? "#2ADA6E" : "#C7D2DB"}
+                fill={state ? "var(--color-green)" : "var(--color-light-blue)"}
                 d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z"
             />
         </svg>
@@ -100,7 +108,7 @@ const Answer = ({ state }: IconProps) => {
             width="50%"
             height="50%"
         >
-            <g fill={state ? "#41BCFF" : "#C7D2DB"}>
+            <g fill={state ? "var(--color-blue)" : "var(--color-light-blue)"}>
                 <path
                     fillRule="evenodd"
                     d="M9 1.5H5.625c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5m6.61 10.936a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 14.47a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094z"
