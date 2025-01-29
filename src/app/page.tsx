80d00b8c6e9fcd4cbeda345e1524de60a9f33cc8
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-01-29 20:19:51 +0900

    コンクリクト解消できてなかった

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,10 @@ import { Input } from "@/components/elements/Input";
 import { PhaseIcon } from "@/components/elements/PhaseIcon";
 import { SideMenu } from "@/components/layout/SideMenu";
 import { useState } from "react";
+
 export default function Home() {
     const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
         <div className={styles.page}>
             <main className={styles.main}>
@@ -50,6 +52,20 @@ export default function Home() {
                     >
                         Read our docs
                     </a>
+                </div>
+                <Input placeholder={"あいことばを入力"} iconUrl="/icon.png" />
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "30px",
+                    }}
+                >
+                    <PhaseIcon phase="read" isOn={true} />
+                    <PhaseIcon phase="delete" isOn={true} />
+                    <PhaseIcon phase="fix" isOn={true} />
+                    <PhaseIcon phase="answer" isOn={true} />
+                    <SideMenu isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
                 </div>
             </main>
             <footer className={styles.footer}>
@@ -96,67 +112,6 @@ export default function Home() {
                     Go to nextjs.org →
                 </a>
             </footer>
-            <Input placeholder={"あいことばを入力"} iconUrl={1} />
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: "30px",
-                }}
-            >
-                <PhaseIcon phase="read" isOn={true} />
-                <PhaseIcon phase="delete" isOn={true} />
-                <PhaseIcon phase="fix" isOn={true} />
-                <PhaseIcon phase="answer" isOn={true} />
-                <SideMenu isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
-            </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-      <Input placeholder={"あいことばを入力"} iconUrl={1} />
-    </div>
-  );
+    );
 }
