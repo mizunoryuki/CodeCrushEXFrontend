1c4620a2d4aa89e9546c8c625718e3a2d38a1526
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-02-01 20:29:06 +0900

    Stateであいことばの入力を管理するようにした

--- src/components/layout/JoinRoom.tsx
+++ src/components/layout/JoinRoom.tsx
@@ -2,8 +2,16 @@ import { Input } from "../elements/Input";
 import { TextButton } from "../elements/TextButton";
 import styles from "../../app/matching/page.module.scss";
 import joinRoom from "../layout/JoinRoom.module.scss";
+import { useState } from "react";
+
+type Watchword = string | number;
 
 export const JoinRoom = () => {
+    const [watchwordToJoin, setWatchwordToJoin] = useState<Watchword>("aiueo");
+
+    const handleChange = (value: Watchword) => {
+        setWatchwordToJoin(value);
+    };
     return (
         <div className={`${styles.contentBox} ${joinRoom.joinBox}`}>
             <div className={joinRoom.watchwordBox}>
@@ -12,9 +20,14 @@ export const JoinRoom = () => {
                     <br />
                     同じ合言葉を入力したプレイヤー同士がマッチングします。
                 </p>
-                <Input placeholder="あいことばを入力" iconUrl="/icon.png" />
+                <Input
+                    placeholder="あいことばを入力"
+                    iconUrl="/icon.png"
+                    onChange={handleChange}
+                />
             </div>
             <TextButton color="green">参加</TextButton>
+            <p>{watchwordToJoin}</p>
         </div>
     );
 };
