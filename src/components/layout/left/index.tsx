ef7d20a498fb7d6581d67589ce9eb6a8dd946a92
kinako1415 <Owata09@outlook.jp>
2025-02-03 19:07:10 +0900

    leftコンポーネントの作成

---
+++ src/components/layout/left/index.tsx
@@ -0,0 +1,14 @@
+import { Memo } from "./Memo";
+import { Phase } from "./Phase";
+import { Timer } from "./Timer";
+import styles from "./index.module.scss";
+
+export const Left = () => {
+  return (
+    <div className={styles.left}>
+      <Phase />
+      <Timer />
+      <Memo />
+    </div>
+  );
+};
