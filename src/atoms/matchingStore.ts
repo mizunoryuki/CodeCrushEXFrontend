89b65573a70fab68279728dd3ebb46eeedf13e6c
kinako1415 <Owata09@outlook.jp>
2025-02-05 04:33:03 +0900

    編集

--- src/atoms/matchingStore.ts
+++ src/atoms/matchingStore.ts
@@ -1,10 +1,9 @@
 import { atom } from "jotai";
 
 type MatchState = "select" | "create" | "join";
-type WatchWord = string | number;
 
 //matching画面のコンポーネント変化のためのステータス
 export const matchingStatusAtom = atom<MatchState>("select");
 
 //あいことば
-export const watchWordAtom = atom<WatchWord>(123456);
+export const watchWordAtom = atom<string>("123456");
