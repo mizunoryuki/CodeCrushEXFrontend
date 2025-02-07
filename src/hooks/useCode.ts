e86bc252f42566f61310021a979111263cf02bef
ayu <umaminokiami@gmail.com>
2025-02-08 02:20:05 +0900

    add:コードの交換

--- src/hooks/useCode.ts
+++ src/hooks/useCode.ts
@@ -1,15 +1,18 @@
+import { myCodeAtom } from "@/atoms/codeStore";
+import { useAtom } from "jotai";
 import { useState, useEffect } from "react";
 
 const useCode = (watchword: string, player: string) => {
+  const [code, setCode] = useAtom(myCodeAtom);
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
   const url = `${apiKey}sse/${watchword}/${player}/codeGet`;
-  const [code, setCode] = useState<string>("code");
+  // const [code, setCode] = useState<string>("code");
 
   useEffect(() => {
     const eventSource = new EventSource(url);
 
     eventSource.onmessage = (event) => {
-      console.log(`sse: ${event.data}`);
+      console.log(`useCode_SSE: ${event.data}`);
       setCode(event.data);
     };
 
